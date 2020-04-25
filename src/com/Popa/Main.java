f72ba44f0ea174aabdd73d0ed2035f54c8d76d67
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-25 12:24:12 +0200

    Spelling errors

--- src/com/Popa/Main.java
+++ src/com/Popa/Main.java
@@ -3,12 +3,12 @@ package com.Popa;
 public class Main {
 
     public static void main(String[] args) {
-        String[][] CountryAndCity = {{"France ", "Sweden "},
+        String[][] countryAndCity = {{"France ", "Sweden "},
                 {"Paris ", "Stockholm "}};
 
-        System.out.println(CountryAndCity[0][0] + CountryAndCity[1][0]);
+        System.out.println(countryAndCity[0][0] + countryAndCity[1][0]);
 
-        System.out.println(CountryAndCity[0][1] + CountryAndCity[1][1]);
+        System.out.println(countryAndCity[0][1] + countryAndCity[1][1]);
 
     }
 }
