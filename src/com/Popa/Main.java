9b059e9b8712598cb826c51c490dd3da73941e38
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-25 09:50:26 +0200

    Initial commit

---
+++ src/com/Popa/Main.java
@@ -0,0 +1,16 @@
+package com.Popa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[][] CountryAndCity = {{"France ", "Sweden "},
+                {"Paris ", "Stockholm "}};
+
+        System.out.println(CountryAndCity[0][0] + CountryAndCity[1][0]);
+
+        System.out.println(CountryAndCity[0][1] + CountryAndCity[1][1]);
+
+    }
+}
+
+
